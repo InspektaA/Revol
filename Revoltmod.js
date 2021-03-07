c6a08baaf1ff882b763c18ca2690f170c659dc1d
Revcy <63965069+Revcy@users.noreply.github.com>
2021-03-07 19:27:45 +0300

    Update Revoltmod.js

--- Revoltmod.js
+++ Revoltmod.js
@@ -7,8 +7,10 @@ Cheat.Print("[+] I remove hotkey list and now it shown in indicator" + " \n");
 Cheat.Print("[+] Reworked watermark (now shown gamerules and alpha-channel support) " + " \n");
 Cheat.Print("[+] New Damage Override and Low Delta" + " \n");
 Cheat.Print("[+] Little Changed Clantag and Ragelog" + " \n");
-Cheat.Print("------------ Changelog 27.02.2021 18:45 ------------" + " \n");
+Cheat.Print("------------ Changelog 06.03.2021 20:10 ------------" + " \n");
 Cheat.Print("[+] Fixed Lowdelta" + " \n");
+Cheat.Print("------------ Changelog 07.03.2021 19:26 ------------" + " \n");
+Cheat.Print("[+] Add AntiBruteforce (from https://pastebin.com/uhv1Bstq)" + " \n");
 
 
 // -- UI buttons --
@@ -17,6 +19,7 @@ UI.AddLabel("-------------------------------");
 UI.AddSliderInt("Antiaim X", 0, screen_size[0]);
 UI.AddSliderInt("Antiaim Y", 0, screen_size[1]);
 UI.AddLabel("-------------------------------");
+UI.AddDropdown("Anti Bruteforce", ["Off", "On Hit", "On Shot"]);
 UI.AddCheckbox("Low delta");
 UI.AddHotkey("Legit AA Key");
 UI.AddCheckbox("Clantag");
@@ -614,6 +617,209 @@ function low_delta()
     }
 }
 
+// -- AntiBruteforce (copyrighted) --
+
+function GetScriptOption(name)
+{
+    var Value = UI.GetValue("Misc", "JAVASCRIPT", "Script Items", name);
+    return Value;
+}
+ 
+function radian(degree)
+{
+    return degree * Math.PI / 180.0;
+}
+ 
+function ExtendVector(vector, angle, extension)
+{
+    var radianAngle = radian(angle);
+    return [extension * Math.cos(radianAngle) + vector[0], extension * Math.sin(radianAngle) + vector[1], vector[2]];
+}
+ 
+function VectorAdd(a, b)
+{
+    return [a[0] + b[0], a[1] + b[1], a[2] + b[2]];
+}
+ 
+function VectorSubtract(a, b)
+{
+    return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+ 
+function VectorMultiply(a, b)
+{
+    return [a[0] * b[0], a[1] * b[1], a[2] * b[2]];
+}
+ 
+function VectorLength(x, y, z)
+{
+    return Math.sqrt(x * x + y * y + z * z);
+}
+ 
+function VectorNormalize(vec)
+{
+    var length = VectorLength(vec[0], vec[1], vec[2]);
+    return [vec[0] / length, vec[1] / length, vec[2] / length];
+}
+ 
+function VectorDot(a, b)
+{
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+ 
+function VectorDistance(a, b)
+{
+    return VectorLength(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
+}
+ 
+function ClosestPointOnRay(target, rayStart, rayEnd)
+{
+    var to = VectorSubtract(target, rayStart);
+    var dir = VectorSubtract(rayEnd, rayStart);
+    var length = VectorLength(dir[0], dir[1], dir[2]);
+    dir = VectorNormalize(dir);
+ 
+    var rangeAlong = VectorDot(dir, to);
+    if (rangeAlong < 0.0)
+    {
+        return rayStart;
+    }
+    if (rangeAlong > length)
+    {
+        return rayEnd;
+    }
+    return VectorAdd(rayStart, VectorMultiply(dir, [rangeAlong, rangeAlong, rangeAlong]));
+}
+ 
+function Flip()
+{
+    UI.ToggleHotkey("Anti-Aim", "Fake angles", "Inverter");
+}
+ 
+var lastHitTime = 0.0;
+var lastImpactTimes =
+[
+    0.0
+];
+var lastImpacts =
+[
+    [0.0, 0.0, 0.0]
+];
+ 
+function OnHurt()
+{
+    if (GetScriptOption("Anti Bruteforce") == 0) return;
+    if (Entity.GetEntityFromUserID(Event.GetInt("userid")) !== Entity.GetLocalPlayer()) return;
+    var hitbox = Event.GetInt('hitgroup');
+ 
+    if (hitbox == 1 || hitbox == 6 || hitbox == 7) 
+    {
+        var curtime = Global.Curtime();
+        if (Math.abs(lastHitTime - curtime) > 0.5)  
+        {
+            lastHitTime = curtime;
+            Flip();
+        }
+    }
+}
+ 
+function OnBulletImpact()
+{
+    if (GetScriptOption("Anti Bruteforce") !== 2) return;
+ 
+    var curtime = Global.Curtime();
+    if (Math.abs(lastHitTime - curtime) < 0.5) return;
+ 
+    var entity = Entity.GetEntityFromUserID(Event.GetInt("userid"));
+    var impact = [Event.GetFloat("x"), Event.GetFloat("y"), Event.GetFloat("z"), curtime];
+    var source;
+    if (Entity.IsValid(entity) && Entity.IsEnemy(entity))
+    {
+        if (!Entity.IsDormant(entity))
+        {
+            source = Entity.GetEyePosition(entity);
+        }
+        else if (Math.abs(lastImpactTimes[entity] - curtime) < 0.1)
+        {
+            source = lastImpacts[entity];
+        }
+        else
+        {
+            lastImpacts[entity] = impact;
+            lastImpactTimes[entity] = curtime;
+            return;
+        }
+        var local = Entity.GetLocalPlayer();
+        var localEye = Entity.GetEyePosition(local);
+        var localOrigin = Entity.GetProp(local, "CBaseEntity", "m_vecOrigin");
+        var localBody = VectorMultiply(VectorAdd(localEye, localOrigin), [0.5, 0.5, 0.5]);
+ 
+        var bodyVec = ClosestPointOnRay(localBody, source, impact);
+        var bodyDist = VectorDistance(localBody, bodyVec);
+       
+        if (bodyDist < 128.0)      
+        {
+            var realAngle = Local.GetRealYaw();
+            var fakeAngle = Local.GetFakeYaw();
+ 
+            var headVec = ClosestPointOnRay(localEye, source, impact);
+            var headDist = VectorDistance(localEye, headVec);
+            var feetVec = ClosestPointOnRay(localOrigin, source, impact);
+            var feetDist = VectorDistance(localOrigin, feetVec);
+ 
+            var closestRayPoint;
+            var realPos;
+            var fakePos;
+ 
+            if (bodyDist < headDist && bodyDist < feetDist)     
+            {                                                      
+                closestRayPoint = bodyVec;
+                realPos = ExtendVector(bodyVec, realAngle + 180.0, 10.0);
+                fakePos = ExtendVector(bodyVec, fakeAngle + 180.0, 10.0);
+            }
+            else if (feetDist < headDist)                      
+            {                                                   
+                closestRayPoint = feetVec;
+                var realPos1 = ExtendVector(bodyVec, realAngle - 30.0 + 90.0, 10.0);
+                var realPos2 = ExtendVector(bodyVec, realAngle - 30.0 - 90.0, 10.0);
+                var fakePos1 = ExtendVector(bodyVec, fakeAngle - 30.0 + 90.0, 10.0);
+                var fakePos2 = ExtendVector(bodyVec, fakeAngle - 30.0 - 90.0, 10.0);
+                if (VectorDistance(feetVec, realPos1) < VectorDistance(feetVec, realPos2))
+                {
+                    realPos = realPos1;
+                }
+                else
+                {
+                    realPos = realPos2;
+                }
+                if (VectorDistance(feetVec, fakePos1) < VectorDistance(feetVec, fakePos2))
+                {
+                    fakePos = fakePos1;
+                }
+                else
+                {
+                    fakePos = fakePos2;
+                }
+            }
+            else                                              
+            {
+                closestRayPoint = headVec;
+                realPos = ExtendVector(bodyVec, realAngle, 10.0);
+                fakePos = ExtendVector(bodyVec, fakeAngle, 10.0);
+            }
+ 
+            if (VectorDistance(closestRayPoint, fakePos) < VectorDistance(closestRayPoint, realPos))      
+            {
+                lastHitTime = curtime;
+                Flip();
+            }
+        }
+ 
+        lastImpacts[entity] = impact;
+        lastImpactTimes[entity] = curtime;
+    }
+}
+
 // -- Watermark --
 
 function watermark() 
@@ -675,6 +881,9 @@ function unloading()
 }
 
 // -- Callback's --
+ 
+Cheat.RegisterCallback("player_hurt", "OnHurt");
+Cheat.RegisterCallback("bullet_impact", "OnBulletImpact");
 Cheat.RegisterCallback("Draw", "main_aa")
 Cheat.RegisterCallback("CreateMove", "low_delta");
 Cheat.RegisterCallback("FrameStageNotify", "aspectration");
@@ -686,4 +895,5 @@ Cheat.RegisterCallback("Draw", "watermark");
 Cheat.RegisterCallback("Unload", "unloading");
 Global.RegisterCallback("CreateMove", "weapons_dmg_override");
 Cheat.RegisterCallback("Draw", "revolt_indicators");
+
 
